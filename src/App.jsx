d3de56665f5890b7e95f35a2e2aa57a85847c0e8
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-03-28 11:40:38 +0600

    Now, hero section add .

--- src/App.jsx
+++ src/App.jsx
@@ -40,8 +40,19 @@ function App() {
         </div>
       </header>
 
+      
+
       {/* main */}
       <main className="max-w-300 mx-auto px-4 lg:px-0 mt-4">
+        {/* hero section */}
+        <div className="bg-black flex flex-col justify-center items-center gap-3 px-4 py-10 rounded-2xl mt-10 mb-20">
+          <img src="https://i.ibb.co.com/FLn9PVxQ/banner-main.png" alt="" />
+          <h1 className="font-bold text-[30px] md:text-[32px] lg:text-[40px] text-white">Assemble Your Ultimate Dream 11 Cricket Team</h1>
+          <p className="text-white/70 text-base md:text-2xl mb-2">Beyond Boundaries Beyond Limits</p>
+          <div className="border border-[#E7FE29] size-fit p-2 rounded-xl">
+            <button className={`py-2 px-5 rounded-xl transition-all bg-[#E7FE29] font-semibold `}>Claim Free Credit</button>
+          </div>
+        </div>
         {/* available and selected */}
         <div className="flex flex-col md:flex-row gap-5 justify-between items-center my-7">
           <h2 className="text-xl md:text-2xl lg:text-[28px] font-bold">
@@ -57,7 +68,7 @@ function App() {
               Available
             </button>
             <button onClick={() => setToggle(false)} className={`py-2 px-5 rounded-r-2xl transition-all ${toggle ? 'text-gray-400 bg-white' : 'bg-[#E7FE29]'} font-semibold `}>
-              Selected ({choosePlayers.length}/{playersData.length})
+              Selected ({choosePlayers.length})
             </button>
           </div>
         </div>
